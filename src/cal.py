eaf1abfcf0dcae53eca4ee41d2535b1ab67ffd86
Krishna C. Garikipati <gkchai@eecs.umich.edu>
2015-08-07 20:09:06 -0400

    calendar integration part 1

--- src/cal.py
+++ src/cal.py
@@ -6,10 +6,14 @@ from email.Utils import COMMASPACE, formatdate
 from email import Encoders
 import os,datetime
 import json, requests
-
+import authomatic
+from authomatic import Authomatic
+from authomatic.providers import oauth2
+import random, string
+import pdb
 CRLF = "\r\n"
 
-login = "busybob15@gmail.com"
+login = "sara@autoscientist.com"
 password = "autoscientist"
 
 def send_invite(from_addr, to_addrs, location, ddtstart, ddtend):
@@ -18,6 +22,7 @@ def send_invite(from_addr, to_addrs, location, ddtstart, ddtend):
     dtstart = ddtstart.strftime("%Y%m%dT%H%M%SZ")
     dtend = ddtend.strftime("%Y%m%dT%H%M%SZ")
     organizer = "ORGANIZER;CN=Sara:mailto:sara@autoscientist.com"
+    # organizer = "ORGANIZER;CN=Krishna:mailto:gkciitd@gmail.com"
 
     description = "DESCRIPTION: Meeting Invite"+CRLF
     attendee = ""
@@ -28,9 +33,9 @@ def send_invite(from_addr, to_addrs, location, ddtstart, ddtend):
     ical+="METHOD:REQUEST"+CRLF+"BEGIN:VEVENT"+CRLF+"DTSTART:"+dtstart+CRLF+"DTEND:"+dtend+CRLF+"DTSTAMP:"+dtstamp+CRLF+organizer+CRLF
     ical+= "UID:FIXMEUID"+dtstamp+CRLF
     ical+= attendee+"CREATED:"+dtstamp+CRLF+description+"LAST-MODIFIED:"+dtstamp+CRLF+"LOCATION:"+CRLF+"SEQUENCE:0"+CRLF+"STATUS:CONFIRMED"+CRLF
-    ical+= "SUMMARY:Meeting @"+location+CRLF+"TRANSP:OPAQUE"+CRLF+"END:VEVENT"+CRLF+"END:VCALENDAR"+CRLF
+    ical+= "SUMMARY:Meeting@"+location+CRLF+"TRANSP:OPAQUE"+CRLF+"END:VEVENT"+CRLF+"END:VCALENDAR"+CRLF
 
-    eml_body = "Email body visible in the invite of outlook and outlook.com but not google calendar"
+    eml_body = "Body of this calendar invite should be visible!"
     eml_body_bin = "This is the email body in binary - two steps"
     msg = MIMEMultipart('mixed')
     msg['Reply-To']=from_addr
@@ -63,50 +68,84 @@ def send_invite(from_addr, to_addrs, location, ddtstart, ddtend):
     mailServer.close()
 
 
+from pymongo import MongoClient
+client = MongoClient() # get a client
+db = client.sara.login # get the database, like a table in sql
+CONFIG = {
+    'google': {
+        'class_': oauth2.Google,
+        'consumer_key': '858092596350-33k2ig53prtk6hct298q4lk8i4o01ubn.apps.googleusercontent.com',
+        'consumer_secret': 'V4LROfAEKq7N7GZcKeeIK1rV',
+        'id': 1,
+        'scope': oauth2.Google.user_info_scope + ['https://www.googleapis.com/auth/calendar'],
+        # '_apis': {
+        #     'List your calendars': ('GET', 'https://www.googleapis.com/calendar/v3/users/me/calendarList'),
+        #     },
+      },
+}
+auth_inst = Authomatic(CONFIG, random.choice(string.ascii_letters))
+
+
 def get_free_slots(addr):
-    ddtstart = datetime.datetime.now()
-    ddtend = ddtstart + datetime.timedelta(days = 60)
-    request_body =  {
-                  "timeMin": ddtstart.strftime("%Y-%m-%dT%H:%M:%SZ"),
-                  "timeMax": ddtend.strftime("%Y-%m-%dT%H:%M:%SZ"),
-                  "items": [
-                    {
-                      "id": addr
+
+    record = db.find_one({'email_id': addr})
+    if record is None:
+        print "Record does not exist for", addr
+        return
+    else:
+
+
+        ddtstart = datetime.datetime.now()
+        ddtend = ddtstart + datetime.timedelta(days = 60)
+        request_body =  {
+                      "timeMin": ddtstart.strftime("%Y-%m-%dT%H:%M:%SZ"),
+                      "timeMax": ddtend.strftime("%Y-%m-%dT%H:%M:%SZ"),
+                      "items": [
+                        {
+                          "id": addr
+                        }
+                      ]
                     }
-                  ]
-                }
 
-    headers = {'content-type': 'application/json; charset=utf-8'}
-    r = requests.post("https://www.googleapis.com/calendar/v3/freeBusy?fields=calendars%2CtimeMax%2CtimeMin&key=AIzaSyA7_jvVXvsBUR85WFuMX_esU2Rgbyg0zpU", data=json.dumps(request_body), headers=headers)
+        headers = {'content-type': 'application/json; charset=utf-8'}
 
-    fs_st, fs_en = [ddtstart],[]
-    bs = []
-    for item in r.json()['calendars'][addr]['busy']:
-        bts = datetime.datetime.strptime(item['start'], "%Y-%m-%dT%H:%M:%SZ")
-        bte = datetime.datetime.strptime(item['end'], "%Y-%m-%dT%H:%M:%SZ")
+        resp = auth_inst.access(record['credentials'],'https://www.googleapis.com/calendar/v3/freeBusy?fields=calendars%2CtimeMax%2CtimeMin&key=AIzaSyAHNN0vG1zmUpQyHeMVyWCV3IZbM-HDoWY', method='POST', body=json.dumps(request_body), headers=headers)
 
-        fs_en.append(bts - datetime.timedelta(milliseconds=0))
-        fs_st.append(bte + datetime.timedelta(milliseconds=0))
+        # r = requests.post("https://www.googleapis.com/calendar/v3/freeBusy?fields=calendars%2CtimeMax%2CtimeMin&key=AIzaSyAHNN0vG1zmUpQyHeMVyWCV3IZbM-HDoWY", data=json.dumps(request_body), headers=headers)
 
-        bs.append((bts,bte))
+        if resp.status == 200:
+            fs_st, fs_en = [ddtstart],[]
+            bs = []
+            for item in json.loads(resp.content)['calendars'][addr]['busy']:
+                bts = datetime.datetime.strptime(item['start'], "%Y-%m-%dT%H:%M:%SZ")
+                bte = datetime.datetime.strptime(item['end'], "%Y-%m-%dT%H:%M:%SZ")
 
-    #add last element of fs_en
-    fs_en.append(ddtend)
+                fs_en.append(bts - datetime.timedelta(milliseconds=0))
+                fs_st.append(bte + datetime.timedelta(milliseconds=0))
 
-    # merge fs_st, fs_en and pair them
-    fs_obj = []
-    for idx, item in enumerate(fs_st):
-        if fs_en[idx] > fs_st[idx] + datetime.timedelta(seconds=1):
-            fs_obj.append({
-                        "start": fs_st[idx],
-                        "end": fs_en[idx]
-                        })
-    return fs_obj
+                bs.append((bts,bte))
+
+            #add last element of fs_en
+            fs_en.append(ddtend)
+
+            # merge fs_st, fs_en and pair them
+            fs_obj = []
+            for idx, item in enumerate(fs_st):
+                if fs_en[idx] > fs_st[idx] + datetime.timedelta(seconds=1):
+                    fs_obj.append({
+                                "start": fs_st[idx],
+                                "end": fs_en[idx]
+                                })
+            return fs_obj
+        else:
+            print "Cannot fetch calendar content"
+            return
+
 
 if __name__ == '__main__':
 
-    attendees = ["freetom15@gmail.com", "freejon15@gmail.com", "gkciitd@gmail.com", 'anandacontact@gmail.com']
-    fro = "Bob Busy<busybob15@gmail.com>"
+    attendees = ["freetom15@gmail.com", "freejon15@gmail.com", "gkciitd@gmail.com",]# 'anandacontact@gmail.com']
+    fro = "Sara<sara@autoscientist.com>"
 
 
     fs = get_free_slots(login)
